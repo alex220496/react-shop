bca4105d61fb413cee0729a5a85cc4846518be98
Alex <focus2204@ukr.net>
2021-04-11 13:49:31 +0300

    aded props

--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -4,20 +4,27 @@ import products from './products'
 
 
 
-const ProductsList = () => {
+const ProductsList = ({}) => {
     return(
         <>
             <h1 className="page-title">Products</h1>
             <div className="row">  
                     {
-                        products.map(products => (
-                            <div className="col-lg-6" key={products.id}>
+                        products.map(({
+                            id,
+                            name,
+                            description,
+                            type,
+                            capacity,
+                            price
+                        }) => (
+                            <div className="col-lg-6" key={id}>
                                 <ProductsListItem
-                                    name={products.name}
-                                    description={products.description}
-                                    type={products.type}
-                                    capacity={products.capacity}
-                                    price={products.price}
+                                    name={name}
+                                    description={description}
+                                    type={type}
+                                    capacity={capacity}
+                                    price={price}
                                 />     
                             </div>
                         ))
--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -1,18 +1,51 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import "./productListItem.css"
 
-const  ProductsListItem = (props) => {
-    console.log(props);
+
+// const user = {
+//     name: "jack",
+//     age: 25
+// }
+
+// const {name, age} = user;
+// console.log(name);
+// console.log(age);
+
+
+
+
+const  ProductsListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price
+
+}) => {
     return (
         <div className="product-list-item">
-            <div className="product-title">{props.name}</div>
-            <div className="product-description">{props.description}</div>
-            <div className="product-features">{props.type}</div>
-            <div className="product-features">{props.capacity} Gb</div>
-            <div className="product-price">{props.price}$</div>
+            <div className="product-title">{name}</div>
+            <div className="product-description">{description}</div>
+            <div className="product-features">{type}</div>
+            <div className="product-features">{capacity} Gb</div>
+            <div className="product-price">{price}$</div>
             <div className="btn-add-to-cart">add to cart</div>
         </div>
     )
 }
+
+ProductsListItem.propTypes = {
+    name:PropTypes.string.isRequired,
+    description:PropTypes.string,
+    type:PropTypes.string.isRequired,
+    capacity:PropTypes.number.isRequired,
+    price:PropTypes.number.isRequired
+}
+
+ProductsListItem.defaultProps = {
+    description: "No description."
+}
+
 
 export default ProductsListItem
