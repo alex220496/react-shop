884af4e1c98cc3288ff0926f617555fb60c319b4
Alex <focus2204@ukr.net>
2021-04-17 12:33:12 +0300

    aded input plus and minus

--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import "./productListItem.css"
 
@@ -12,39 +12,57 @@ import "./productListItem.css"
 // console.log(name);
 // console.log(age);
 
+class ProductsListItem extends Component {
+    
+    state = {
+        productCount:5,
+    }
 
+    onIncrementClick () {
+        this.setState((prevState) =>({
+            productCount:prevState.productCount + 1
+        }))
+    }
 
+    onDecrementClick () {
+        this.setState((prevState) =>({
+            productCount:prevState.productCount - 1
+        }))
+    }
 
-const  ProductsListItem = ({
-    name,
-    description,
-    type,
-    capacity,
-    price,
-    image
+    render() {
+        const {
+            name,
+            description,
+            type,
+            capacity,
+            price,
+            image
+        } = this.props
 
-}) => {
-    return (
-        <div className="product-list-item">
-            <div className="product-img">
-                <img src={image} alt=""/>
+        return (
+            <div className="product-list-item">
+                <div className="product-img">
+                    <img src={image} alt=""/>
+                </div>
+                <div className="product-title">{name}</div>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity} Gb</div>
+                <div className="product-price">{price}$</div>
+                
+                 <div className="product-quantity">
+                     <button onClick={() => this.onDecrementClick()}>-</button>
+                     <input type="text" value={this.state.productCount} readOnly/>
+                     <button onClick={() => this.onIncrementClick()}>+</button>
+                 </div>
+    
+                <div className="btn-add-to-cart">add to cart</div>
             </div>
-            <div className="product-title">{name}</div>
-            <div className="product-description">{description}</div>
-            <div className="product-features">{type}</div>
-            <div className="product-features">{capacity} Gb</div>
-            <div className="product-price">{price}$</div>
-
-             <div className="product-quantity">
-                 <button>-</button>
-                 <input type="text" value="1"/>
-                 <button>+</button>
-             </div>
-             
-            <div className="btn-add-to-cart">add to cart</div>
-        </div>
-    )
+        )
+    }
 }
+
 
 ProductsListItem.propTypes = {
     name:PropTypes.string.isRequired,
