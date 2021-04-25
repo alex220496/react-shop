5b578be686c9b4ccbe091c4a97793bc9ce23d199
Alex <focus2204@ukr.net>
2021-04-25 20:29:24 +0300

    aded new style for h1

--- src/App/Main/CartPage/CartPage.js
+++ src/App/Main/CartPage/CartPage.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import {keys} from 'lodash'
+import products from '../Products/products'
 
-const CartPage = () => {
+const productsObj = products.reduce((obj, product) =>({
+    ...obj,
+    [product.id]:product
+}),{})
+
+const CartPage = ({
+    productsInCart
+}) => {
     return (
         <>
         
             <h1 className="page-title">Cart</h1>
+            <div>
+                {
+                    keys(productsInCart).map(productId =>(
+                        <div key={productId}> {productsObj[productId].name} 
+                        : {productsInCart[productId]}</div>
+                    ))    
+                }
+            </div>
+
+            <div>
+                Total: {
+                    keys(productsInCart).reduce((total,productId) =>(
+                        total + (productsObj[productId].price * productsInCart[productId])
+                    ), 0)
+                } 
+            </div>
         
         </>
     )
--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProductsListItem from './ProductsListItem'
 import products from './products'
+import "./productListItem.css"
 
 
 
